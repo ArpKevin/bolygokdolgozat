d9f6836172b1a9fc7e9760d9bf5ba22e366e70d5
c20árpásk <c20rpsk@203-02>
2023-10-24 13:06:11 +0200

    Add project files.

---
+++ bolygokdolgozat/Bolygo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bolygokdolgozat
+{
+    class Bolygo
+    {
+        public string Nev { get; set; }
+        public int HoldakSzama { get; set; }
+        public double TerfogatArany { get; set; }
+
+        public Bolygo(string sor) {
+            var b = sor.Split(";");
+            Nev = b[0];
+            HoldakSzama = int.Parse(b[1]);
+            TerfogatArany = double.Parse(b[2].Replace(".", ","));
+        }
+    }
+}
---
+++ bolygokdolgozat/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace bolygokdolgozat
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var bolygok = new List<Bolygo>();
+
+            using var sr = new StreamReader(@"..\..\..\src\solsys.txt");
+
+            while (!sr.EndOfStream)
+            {
+                bolygok.Add(new Bolygo(sr.ReadLine()));
+            }
+
+            Console.WriteLine("3.feladat:");
+
+            Console.WriteLine($"\t3.1: {bolygok.Count} bolygó van a naprendszerben");
+
+            Console.WriteLine($"\t3.2: Egy naprendszerben egy bolygónak átlagosan {bolygok.Average(b => b.HoldakSzama)} holdja van");
+
+            Console.WriteLine($"\t3.3: A legnagyobb térfogató bolygó a {bolygok.MaxBy(b => b.TerfogatArany).Nev}");
+
+            Console.Write("\t3.4: írd be a keresett bolygó nevét: ");
+            string bolygoInput = Console.ReadLine();
+
+            Console.WriteLine($"\t\t{(bolygok.Exists(b => b.Nev == bolygoInput) ? "van" : "sajnos nincs")} ilyen bolygó a naprendszerben");
+
+            Console.Write("\t3.5 Írj be egy számot: ");
+            int bekertSzam = int.Parse(Console.ReadLine());
+
+            string[] holdasBolygok = bolygok.Where(b => b.HoldakSzama > bekertSzam).Select(b => b.Nev).ToArray();
+
+            Console.WriteLine($"\t\tA következő bolygóknak van {bekertSzam}-nál/nél több holdja:");
+
+            Console.Write("\t\t");
+
+            string holdayBolygokString = string.Join(", ", holdasBolygok);
+
+            Console.WriteLine(holdayBolygokString);
+        }
+    }
+}
